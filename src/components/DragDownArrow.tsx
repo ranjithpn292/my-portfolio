98de5e3a3dad374f99dab02466bd5ae454bda8c2
Ranjith P N <ranjithpn@Ranjiths-MacBook-Air.local>
2025-09-09 17:23:17 +0530

    lint fix

--- src/components/DragDownArrow.tsx
+++ src/components/DragDownArrow.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import StyledArrow from "./ArrowIcon";
 
 const ArrowWrapper = styled.div<{ visible: boolean }>`
